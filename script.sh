bb65d2a31bbead18e3d355c8070d621c9f81afa2
flaemer <121102701+flaemer@users.noreply.github.com>
2025-07-10 02:21:24 +0700

    script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,18 @@
-!/bin/bash
-#антиэто ну там вроде хз ну анти кто то
-if ! ping -c 1 8.8.8.8 &> /dev/null; then
-    echo "интернет нада братанчик"
-    exit 1
-fi
-### 1. Настройка системы
-echo "установка базы"
-pacstrap /mnt base base-devel linux linux-firmware networkmanager nano git linux-headers
-### 2. Настройка пользователя ###
-read -p "Введите имя пользователя: " username
-useradd -m -G wheel,audio,video,storage,optical -s /bin/bash "$username"
-echo "Установите пароль для $username:"
-read -p "Введите пароль пользователя: " password
-passwd "$password"
-#для субо
-read -sp "Введите пароль для субо братана: " rootpass
-echo "root:$rootpass" | chpasswd
-unset rootpass
-### 3. Настройка sudo
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-echo "все этот ну вот он $username создан и теперь тоже может юзать судо"
-echo "я хз как мне лень виртуалку запусктаь пиши сам arch-chroot /mnt"
+#!/bin/bash
+### 2. Установка базовой системы
+echo -e "\e[32m[2] Устанавливаю базовые пакеты...\e[0m"
+pacstrap /mnt base base-devel linux linux-firmware nano git linux-headers
+
+### 3. Генерация fstab
+echo -e "\e[32m[3] Генерирую fstab...\e[0m"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+### 4. Копируем часть 2 скрипта в /mnt
+echo -e "\e[32m[4] Готовлю часть 2 для chroot...\e[0m"
+curl -o /mnt/script-2.sh https://raw.githubusercontent.com/flaemer/flaemer/script-2.sh
+chmod +x /mnt/script-2.sh
+
+echo -e "\e[32m\nБазовая установка завершена!\e[0m"
+echo -e "Перейдите в chroot и запустите часть 2:"
+echo -e "  arch-chroot /mnt"
+echo -e "  ./script-2.sh"
