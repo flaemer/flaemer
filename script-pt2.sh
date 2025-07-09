b4550f084bc00871e5e6e57cabb706e3a465d2c8
flaemer <121102701+flaemer@users.noreply.github.com>
2025-07-09 12:12:55 +0700

    script-pt2.sh

--- script-pt2.sh
+++ script-pt2.sh
@@ -1,27 +1,5 @@
 !/bin/bash
-#антиэто ну там вроде хз ну анти кто то
-if ! ping -c 1 8.8.8.8 &> /dev/null; then
-    echo "интернет нада братанчик"
-    exit 1
-fi
-### 1. Настройка системы
-echo "установка базы"
-pacstrap /mnt base base-devel linux linux-firmware networkmanager nano git linux-headers
-### 2. Настройка пользователя ###
-read -p "Введите имя пользователя: " username
-useradd -m -G wheel,audio,video,storage,optical -s /bin/bash "$username"
-echo "Установите пароль для $username:"
-read -p "Введите пароль пользователя: " password
-passwd "$password"
-#для субо
-read -sp "Введите пароль для субо братана: " rootpass
-echo "root:$rootpass" | chpasswd
-unset rootpass
-### 3. Настройка sudo
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-echo "все этот ну вот он $username создан и теперь тоже может юзать судо"
-echo "я хз как мне лень виртуалку запусктаь пиши сам arch-chroot /mnt"
-### 4. Установка пакетов (pacman)
+# packages
     git clone https://aur.archlinux.org/yay-bin.git
     cd yay-bin
     makepkg -si --noconfirm
@@ -38,12 +16,12 @@ pacman -S --needed \
 # yay -S --noconfirm --needed adwaita-dark adwaita-qt5 ayugram-desktop-bin elyprismlauncher-bin heroic-games-launcher-bin libasound2-plugin-fftrate libgbinder libglibutil libwireplumber-4.0-compat mobydroid mobydroid-debug portproton protontricks protonup-qt-bin pwvucontrol python-gbinder python-vdf rofi-power-menu ventoy-bin ventoy-bin-debug vesktop-bin waydroid webcord yay yay-debug
 
 
-### 5. Установка загрузчика (GRUB)
+#grub
 echo "egg"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-### 6. запуски всякие
+##памойка
 systemctl enable NetworkManager
 
 #конфиги
@@ -53,5 +31,5 @@ CONFIG_SOURCE="$TMP_DIR/flaemer/.config" # Откуда копировать
 CONFIG_TARGET="/home/$USER/.config" # Куда копировать
 git clone https://github.com/flaemer/flaemer.git $TMP_DIR
 cp -r /home/$username/yaiko/.config /home/$username/
-### 7. Завершение ###
+#раслабление или что то там
 echo "бабах случится завтра я уверен в этом"
