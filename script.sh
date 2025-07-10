fd89d6e40471cacda3e3392fb7b7ad6e7045b8ee
flaemer <121102701+flaemer@users.noreply.github.com>
2025-07-11 03:19:17 +0700

    это ну да обновил эти штуки читать кстати могу ну понятнее мне да да и это еще да вот
    добавил убрал гавна ну и еще вроде я хз забыл уже

--- script.sh
+++ script.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
+
 # reflector yeye
 reflector --country Russia --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-# Package sets
+
+# Packages for pc or laptop
 amd_pc="btop"
-hp_notebook="fastfetch"
+hp_notebook="grub efibootmgr os-prober hyprland rofi swaync lollypop arch-install-scripts radeontop chromium swww yt-dlp steam qbittorrent polkit-gnome playerctl obs-studio networkmanager mpv ly krita kitty kid3 kate gvfs-mtp grim hyprlock flatpak filelight fastfetch efibootmgr grub btop blueman bluez mesa mesa-utils vulkan-radeon lib32-mesa lib32-vulkan-radeon power-profiles-daemon slurp wl-clipboard pipewire pipewire-pulse wireplumber powertop xdg-desktop-portal-hyprland gnu-free-fonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra otf-font-awesome papirus-icon-theme ttf-dejavu ttf-fira-code ttf-fira-sans ttf-firacode-nerd ttf-font-awesome ttf-hack ttf-liberation ttf-nerd-fonts-symbols ttf-opensans ttf-ubuntu-font-family gnome-disk-utility"
 read -p "your name: " USERNAME
 home="/home/$USERNAME"
-# Mount boot razdel
-read -p "Enter boot partition (e.g., /dev/sda1): " boot_efi
-[ -z "$boot_efi" ] && { echo "Boot partition not specified!"; exit 1; }
 
+#разделы эти ну да
+
+# Mount root razdel
+read -p "Enter root part  (e.g., /dev/sda2): " root
+[ -z "$root" ] && { echo "root partition not specified!"; exit 1; }
+mkfs.ext4 "$root"
+mount "$root" /mnt || { echo "Error mounting $root"; exit 1; }
+
+#home razdel
+read -p "Enter home  (e.g., /dev/sda3): " home
+[ -z "$home" ] && { echo "home partition not specified!"; exit 1; }
+mkfs.ext4 "$home"
+mkdir -p /mnt/home
+mount "$home" /mnt/home || { echo "Error mounting $home"; exit 1; }
+
+
+#boot razdel
+read -p "Enter boot part  (e.g., /dev/sda1): " boot_efi
+[ -z "$boot_efi" ] && { echo "Boot partition not specified!"; exit 1; }
+mkfs.vfat "$boot"
 mkdir -p /mnt/boot/efi
 mount "$boot_efi" /mnt/boot/efi || { echo "Error mounting $boot_efi"; exit 1; }
 
-echo "Checking partitions:"
+#lsblk for check
 lsblk | grep -v "loop"
 sleep 2
 
@@ -24,13 +43,14 @@ pacstrap /mnt base base-devel linux linux-firmware nano sudo git linux-headers r
 # Generate fstab
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab || { echo "Failed to generate fstab"; exit 1; }
-# Package selection
-echo "Select system type:"
-echo "1) Laptop ($hp_notebook)"
-echo "2) Desktop ($amd_pc)"
 
+# Package selection
+echo "laptor or desktop?"
+echo "1) Laptop"
+echo "2) Desktop" 
 read -p "Your choice (1-2): " choice
 
+#chroot часть
 arch-chroot /mnt /bin/bash <<EOF
 
 #user
@@ -54,15 +74,25 @@ git clone https://aur.archlinux.org/yay-bin.git ~/$yay &&
 cd ~/yay &&
 makepkg -si --noconfirm
 
-# Copy configs
-echo "Copying configurations..."
-git clone https://github.com/flaemer/flaemer.git $home &&
-echo "installed?"
-    
 #PACKAGES
 case $choice in
-    1) pacman -S $hp_notebook;;
-    2) pacman -S  $amd_pc;;
+    1) pacman -S --noconfirm $hp_notebook;;
+    2) pacman -S --noconfirm $amd_pc;;
     *) echo "Invalid choice, skipping additional packages";;
+#grub памойка эта 
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#еще пакеты эти ну да базовые да да деньги
+# Установка сервисных пакетов
+
+# Настройка сервисов
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl --user enable pipewire pipewire-pulse wireplumber
+systemctl enable power-profiles-daemon
+systemctl enable thermald
+
 esac
 EOF
